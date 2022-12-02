96af5e03428816bb6a1bf0ad90f14c25d1a23e30
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-12-02 10:47:38 +0000

    Added powerfulDigitCounts function at script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,39 @@ function getLengthInfo() {
     // Display Information in the Browser
     document.getElementById("lengthinfo").innerHTML = txt;
 }
+
+/*
+    Function to return the number of positive integers of length n and an
+    nth power
+
+    powerfulDigitCounts(1)  returns 9
+    powerfulDigitCounts(2)  returns 6
+    powerfulDigitCounts(3)  returns 5
+    powerfulDigitCounts(4)  returns 4
+    powerfulDigitCounts(5)  returns 3
+    powerfulDigitCounts(6)  returns 3
+    powerfulDigitCounts(7)  returns 2
+    powerfulDigitCounts(8)  returns 2
+    powerfulDigitCounts(10) returns 2
+    powerfulDigitCounts(21) returns 1
+
+*/
+function powerfulDigitCounts(n) {
+    function countDigits(num) {
+        let counter = 0;
+        while (num>0) {
+            num = Math.floor(num/10);
+            counter++;
+        }
+        return counter;
+    }
+
+    let numbersCount = 0;
+    let curNum = 1;
+    while (curNum < 10) {
+        let power = n;
+        if (power === countDigits(curNum ** power)) numbersCount++;
+    }
+
+    return numbersCount;
+}
